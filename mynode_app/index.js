fc5ed822076c67d1df33abe5431b282297089745
Rafaella Duarte <rafaella.d.d.carvalho@gmail.com>
2025-09-26 21:59:31 -0300

    Squashed 'modulo-01-docker-container/codigo-fonte/docker-pratica/' content from commit c1a0b9a
    git-subtree-dir: modulo-01-docker-container/codigo-fonte/docker-pratica
    git-subtree-split: c1a0b9ab60dc1c93c30e3e48b7099969d0b56e73

---
+++ mynode_app/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const message = process.env.MESSAGE || "Hello World!";
+
+app.get("/", (req, res) => {
+  res.send(message);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running ON port ${port}`);
+});
